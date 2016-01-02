adda93ed65f829eb641e7809c84f82f222d04a1a
Frank Cobia <frank_cobia@me.com>
2016-01-02 11:53:40 -0500

    Initial
    Initial

---
+++ CocoaPodsTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CocoaPodsTest
+//
+//  Created by Frank Cobia on 1/2/16.
+//  Copyright © 2016 Online Data Services, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
